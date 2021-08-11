b1634aade3d8bf936ad351498fc59c95eb09ad56
Fraser <Fraser.Hughes@btinternet.com>
2021-08-11 13:39:48 +0200

    Add code to UI and calculator files.

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -2,8 +2,11 @@ describe('User can check their BMI', () => {
         it('User can enter their weight and height', () => {
                 cy.visit('http://localhost:3474')
                 cy.contains('BMI Calculator')
-                cy.get('#weight').type('78')   
-                cy.get('#height').type('185')
+                cy.get('#weight').type('90')   
+                cy.get('#height').type('186')
                 cy.get('#Calculate').click()
-        });  
+        });
+        it('displays BMI value of 26.01'), () => {
+                cy.get('#result').should('contain', '26.01')
+                        }
 });
---
+++ src/assets/js/bmiCalculator.js
@@ -0,0 +1,5 @@
+const calculateMetricBMI = (weight, height) => {
+  let result
+  result = weight / (height * height)
+  return result
+}
