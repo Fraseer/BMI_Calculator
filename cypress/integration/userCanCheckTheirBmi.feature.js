1ae5f52b6489edb60b7070ccec337971ef847f06
Fraser <Fraser.Hughes@btinternet.com>
2021-08-12 16:22:54 +0200

    Failure of cpyress test

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,12 +1,12 @@
-describe('User can check their BMI', () => {
-        it('User can enter their weight and height', () => {
-                cy.visit('http://localhost:3474')
-                cy.contains('BMI Calculator')
-                cy.get('#weight').type('90')   
-                cy.get('#height').type('186')
-                cy.get('#Calculate').click()
-        });
-        it('displays BMI value of 26.01'), () => {
-                cy.get('#result').should('contain', '26.01')
-                        }
+describe("User can check their BMI", () => {
+  it("User can enter their weight and height", () => {
+    cy.visit("/");
+    cy.contains("BMI Calculator");
+    cy.get("#weight").type("90");
+    cy.get("#height").type("186");
+    cy.get("#calculate").click();
+  });
+  it("is expected to display BMI value of 26.01", () => {
+    cy.get("#results").should("contain", "26.01");
+  });
 });
--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,8 +1,13 @@
-const calculateBMI = () => {
+function calculateBMI() {
   let weightInput = document.getElementById("weight")
   let heightInput = document.getElementById("height")
 
   let results = calculateMetricBMI(weightInput.value, heightInput.value)
-  let displayResultsElement = document.getElementById('results')
-  displayResultsElement.innnerHTML = `<h1>Your BMI value is: ${results}.<h1>`
+
+  let displayResultsElement = document.getElementById("results")
+  debugger
+  displayResultsElement.innerHTML = `<h2> Your BMI value is: ${results}</h2>`
 }
+
+const button = document.getElementById("calculate")
+button.addEventListener("click", calculateBMI())
